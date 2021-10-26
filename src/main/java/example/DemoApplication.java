3371b0259e17a1e4d02ed34977adea0f0eb9aadc
James Little <alphafoobar@users.noreply.github.com>
2021-10-26 23:42:03 +1300

    Don't cache failed DNS lookups
    See https://github.com/brettwooldridge/HikariCP/issues/1075#issuecomment-364973591

--- src/main/java/example/DemoApplication.java
+++ src/main/java/example/DemoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		java.security.Security.setProperty("networkaddress.cache.ttl" , "5");
+		java.security.Security.setProperty("networkaddress.cache.ttl" , "1");
+		// https://github.com/brettwooldridge/HikariCP/issues/1075#issuecomment-364973591
+		java.security.Security.setProperty("networkaddress.cache.negative.ttl" , "0");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
